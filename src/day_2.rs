02ea71d1281fdb82f1d69fd3741ab679430fbdff
reinvantveer <rein@vantveer.me>
2021-12-02 22:25:03 +0100

    add solution for day 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -3,9 +3,13 @@ use advent_of_code_2021::read_lines;
 pub(crate) fn run() {
     let descriptions = read_lines("data/day_2_input.txt");
     let movements = parse_movements(&descriptions);
-    let (horizontal, depth) = calculate_position(movements);
-    println!("Horizontal position is at {}, depth at {}", horizontal, depth);
-    println!("These two multiplied is {}", horizontal * depth);
+    let (horizontal, depth) = calculate_position(&movements);
+    println!("Horizontal position is at {}, depth at {} for part 1", horizontal, depth);
+    println!("These two multiplied is {} for part 1", horizontal * depth);
+
+    let (horizontal, depth) = calculate_position_with_aim(&movements);
+    println!("Horizontal position is at {}, depth at {} for part 2", horizontal, depth);
+    println!("These two multiplied is {} for part 2", horizontal * depth);
 }
 
 pub(crate) struct Movement {
@@ -27,16 +31,36 @@ pub(crate) fn parse_movements(descriptions: &Vec<String>) -> Vec<Movement> {
         .collect()
 }
 
-pub(crate) fn calculate_position(movements: Vec<Movement>) -> (i32, i32) {
+pub(crate) fn calculate_position(movements: &Vec<Movement>) -> (i32, i32) {
     let mut horizontal: i32 = 0;
     let mut depth: i32 = 0;
 
     for movement in movements {
         match movement.direction.as_str() {
             "forward" => horizontal += movement.amount as i32,
-            "backward" => horizontal -= movement.amount as i32,
             "up" => depth -= movement.amount as i32,
             "down" => depth += movement.amount as i32,
+            _ => { unreachable!("Direction was not recognized: {}", movement.direction)},
+        }
+    }
+
+    (horizontal, depth)
+}
+
+pub(crate) fn calculate_position_with_aim(movements: &Vec<Movement>) -> (i32, i32) {
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;
+
+    let mut aim: i32 = 0;
+
+    for movement in movements {
+        match movement.direction.as_str() {
+            "forward" => {
+                horizontal += movement.amount as i32;
+                depth += aim * movement.amount as i32;
+            },
+            "up" => aim -= movement.amount as i32,
+            "down" => aim += movement.amount as i32,
             _ => { unreachable!("Direction was not recognized: {}", movement.direction)},
         }
     }
@@ -51,7 +75,17 @@ fn test_movement() {
     let movements = parse_movements(&movement_descriptions);
     assert_eq!(movements.len(), 6);
 
-    let (horizontal, depth) = calculate_position(movements);
+    let (horizontal, depth) = calculate_position(&movements);
     assert_eq!(horizontal, 15);
     assert_eq!(depth, 10);
+}
+
+#[test]
+fn test_movement_with_aim() {
+    let movement_descriptions = read_lines("data/day_2_sample.txt");
+    let movements = parse_movements(&movement_descriptions);
+
+    let (horizontal, depth) = calculate_position_with_aim(&movements);
+    assert_eq!(horizontal, 15);
+    assert_eq!(depth, 60);
 }
