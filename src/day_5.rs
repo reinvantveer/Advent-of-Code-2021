75f7967c1f58a6a052054aaf59738e3aa13d4cc5
reinvantveer <rein@vantveer.me>
2021-12-05 20:58:21 +0100

    add beginning of line parser
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,3 +1,34 @@
+use advent_of_code_2021::read_lines;
+
 pub(crate) fn run() {
 
+}
+
+type Point = (usize, usize);
+
+pub(crate) fn parse_lines(inputs: &Vec<String>) -> (Vec<Point>, Vec<Point>) {
+    let hori_lines = Vec::new();
+    let verti_lines = Vec::new();
+
+    for input in inputs {
+        let start_end = input
+            .split(" -> ")
+            .collect::<Vec<_>>();
+
+        let start_str = *start_end.get(0).unwrap();
+
+        let start_point = start_str
+            .split(",")
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+    }
+
+    (hori_lines, verti_lines)
+}
+
+#[cfg(test)]
+#[test]
+fn test_straight_line_parser() {
+    let inputs = read_lines("data/day_5_sample.txt");
+    let (hori_lines, vert_lines) = parse_lines(&inputs);
 }
