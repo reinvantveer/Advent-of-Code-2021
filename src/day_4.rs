6a54987ce5474859f2eced13039946424bb6aa4e
reinvantveer <rein@vantveer.me>
2021-12-05 14:05:34 +0100

    finish board parsing

--- src/day_4.rs
+++ src/day_4.rs
@@ -6,30 +6,31 @@ pub(crate) fn run() {
 
 /// Parses the bingo data, consisting of a first line of bingo number calls,
 /// followed by blank-line separated bingo boards
-pub(crate) fn parse_bingo_data(inputs: Vec<String>) -> (Vec<usize>, Vec<Vec<usize>>) {
-    let mut numbers = inputs.iter().next().unwrap();
+pub(crate) fn parse_bingo_data(inputs: Vec<String>) -> (Vec<usize>, Vec<Vec<Vec<usize>>>) {
+    let numbers = inputs.iter().next().unwrap();
     let number_calls = numbers
         .split(",")
         .map(|num| num.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
 
     let mut boards = Vec::new();
-
-    let mut row = 0;
-    let mut column = 0;
     let mut board = Vec::new();
 
-    for line in inputs[1..].iter() {
-        row += 1;
+    for line in inputs[2..].iter() {
+        // Skip empty lines
+        if line.to_string() == "".to_string() { continue; }
 
-        // Reset board
-        if line == &"".to_string() {
+        let numbers = &mut line
+            .split_ascii_whitespace()
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+        // println!("Appending {:?}", &numbers);
+        board.push(numbers.clone());
+
+        if board.len() == 5 {
             boards.push(board);
             board = Vec::new();
-            row = 0;
-            column = 0;
         }
-
     }
 
     (number_calls, boards)
@@ -43,6 +44,17 @@ fn test_bingo_data_parser() {
     assert_eq!(
         number_calls,
         vec![7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
-    )
+    );
+    assert_eq!(boards.len(), 3);
+
+    let first_board = boards.first().unwrap();
+    let last_board = boards.last().unwrap();
+    assert_eq!(first_board.len(), 5);
+    assert_eq!(last_board.len(), 5);
+
+    let first_board_first_row = first_board.first().unwrap();
+    let last_board_last_row = last_board.last().unwrap();
+    assert_eq!(first_board_first_row.len(), 5);
+    assert_eq!(last_board_last_row.len(), 5);
 
 }
