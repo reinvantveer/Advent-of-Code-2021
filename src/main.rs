791ce637b180ee9a9236856f6c848c272c8d9388
reinvantveer <rein@vantveer.me>
2021-12-05 10:32:14 +0100

    bootstrap module for day 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 
 use clap::{App, Arg};
 
@@ -24,6 +25,7 @@ pub fn main() {
         "1" => { day_1::run() },
         "2" => { day_2::run() },
         "3" => { day_3::run() },
+        "4" => { day_4::run() },
         _ => { todo!("This day isn't implemented (yet)") },
     };
 }
