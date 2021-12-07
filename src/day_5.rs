3f229c7eea00abcb12dfe08bc90772c500911ed8
reinvantveer <reinvantveer@gmail.com>
2021-12-07 20:34:16 +0100

    WIP: try & parse diagonal lines
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,15 +1,25 @@
 use advent_of_code_2021::read_lines;
 
 pub(crate) fn run() {
+    let inputs = read_lines("data/day_5_input.txt");
+    let (hor_lines, ver_lines, dia_lines) = parse_lines(&inputs, false);
+    let mut all_lines = hor_lines;
+    all_lines.extend(ver_lines);
+    all_lines.extend(dia_lines);
+    let grid = grid_sum_from_lines(&all_lines);
+    let score = hotspots_count(&grid);
 
+    println!("The total number of crossing horizontal and vertical lines in the grid is {}", &score);
 }
 
 type Point = (usize, usize);
 type Line = Vec<Point>;
+type Grid = Vec<Vec<usize>>;
 
-pub(crate) fn parse_lines(inputs: &Vec<String>) -> (Vec<Line>, Vec<Line>) {
+pub(crate) fn parse_lines(inputs: &Vec<String>, parse_diagonals: bool) -> (Vec<Line>, Vec<Line>, Vec<Line>) {
     let mut hor_lines = Vec::new();
     let mut ver_lines = Vec::new();
+    let mut dia_lines = Vec::new();
 
     for input in inputs {
         let start_end = input
@@ -30,7 +40,7 @@ pub(crate) fn parse_lines(inputs: &Vec<String>) -> (Vec<Line>, Vec<Line>) {
             .collect::<Vec<_>>();
 
         if start_point[0] == end_point[0] {
-            println!("Line is vertical");
+            // println!("Line is vertical");
             // Range over y positions according to direction. Is it away from the origin?
             let range = match end_point[1] > start_point[1] {
                 true => start_point[1]..end_point[1] + 1,
@@ -42,23 +52,127 @@ pub(crate) fn parse_lines(inputs: &Vec<String>) -> (Vec<Line>, Vec<Line>) {
                 .collect::<Line>();
             ver_lines.push(line);
         } else if start_point[1] == end_point[1] {
-            println!("Line is horizontal");
-            let line = (start_point[0]..end_point[0] + 1)
+            // println!("Line is horizontal");
+            let range = match end_point[0] > start_point[0] {
+                true => start_point[0]..end_point[0] + 1,
+                false => end_point[0]..start_point[0] + 1,
+            };
+
+            let line = (range)
                 .map(|x| (x, start_point[1]))
                 .collect::<Line>();
             hor_lines.push(line);
+        } else {
+            if !parse_diagonals { continue; }
+            // println!("Line is diagonal");
+            let range = match end_point[0] > start_point[0] {
+                true => start_point[0]..end_point[0] + 1,
+                false => end_point[0]..start_point[0] + 1,
+            };
+
+            let start_y = start_point[1];
+            let y_is_up = end_point[1] > start_y;
+            let mut y_pos = start_y;
+
+            let line = (range)
+                .map(|x| {
+                    let y = match y_is_up {
+                        true => { y_pos += 1; y_pos }
+                        false => { y_pos -= 1; y_pos }
+                    };
+                    (x, y)
+                })
+                .collect::<Line>();
+            dia_lines.push(line);
         }
     }
 
-    (hor_lines, ver_lines)
+    (hor_lines, ver_lines, dia_lines)
+}
+
+pub(crate) fn grid_sum_from_lines(lines: &Vec<Line>) -> Grid {
+    let mut grid_size = 10;
+    let mut all_points = Vec::new();
+
+    // Calculate grid size from points max
+    // And gather all points in a simpler vec
+    for line in lines {
+        for &point in line {
+            all_points.push(point);
+
+            if point.0 > grid_size {
+                grid_size = point.0;
+            } else if point.1 > grid_size {
+                grid_size = point.1;
+            }
+        }
+    }
+
+    let mut grid: Grid = vec![vec![0_usize; grid_size + 1]; grid_size + 1];
+
+    for point in all_points {
+        grid[point.0][point.1] += 1;
+    }
+
+    grid
+}
+
+pub(crate) fn hotspots_count(grid: &Grid) -> usize {
+    let mut score = 0;
+
+    for line in grid {
+        for entry in line {
+            if *entry > 1 {
+                score += 1;
+            }
+        }
+    }
+
+    score
 }
 
 #[cfg(test)]
 #[test]
 fn test_straight_line_parser() {
     let inputs = read_lines("data/day_5_sample.txt");
-    let (hor_lines, ver_lines) = parse_lines(&inputs);
+    let (hor_lines, ver_lines, dia_lines) = parse_lines(&inputs, false);
+
+    assert_eq!(hor_lines.len(), 4);
+    assert_eq!(ver_lines.len(), 2);
+    assert_eq!(dia_lines.len(), 0);
 
     assert_eq!(hor_lines[0], vec![(0, 9), (1, 9), (2, 9), (3, 9), (4, 9), (5, 9)]);
     assert_eq!(ver_lines[0], vec![(2, 1), (2, 2)]);
+}
+
+#[test]
+fn test_grid_sum_from_lines() {
+    let inputs = read_lines("data/day_5_sample.txt");
+    let (hor_lines, ver_lines, _) = parse_lines(&inputs, false);
+    let mut all_lines = hor_lines;
+    all_lines.extend(ver_lines);
+    let grid = grid_sum_from_lines(&all_lines);
+    assert_eq!(grid[0][0], 0);
+    assert_eq!(grid[3][4], 2);
+}
+
+#[test]
+fn test_number_of_points_where_at_least_two_straight_lines_overlap() {
+    let inputs = read_lines("data/day_5_sample.txt");
+    let (hor_lines, ver_lines, _) = parse_lines(&inputs, false);
+    let mut all_lines = hor_lines;
+    all_lines.extend(ver_lines);
+    let grid = grid_sum_from_lines(&all_lines);
+
+    let score = hotspots_count(&grid);
+    assert_eq!(score, 5);
+}
+
+#[test]
+fn test_diagonal_line_parser() {
+    let inputs = read_lines("data/day_5_sample.txt");
+    let (_, _, dia_lines) = parse_lines(&inputs, true);
+
+    assert_eq!(dia_lines.len(), 4);
+    assert_eq!(dia_lines[0], vec![(8, 0), (7, 1), (6, 2), (5, 3), (4, 4), (3, 5), (2, 6), (1, 7), (0, 8)]);
 }
