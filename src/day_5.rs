8aa82bed31894de35f6a9ae1063ce001395ada47
reinvantveer <reinvantveer@gmail.com>
2021-12-07 17:21:54 +0100

    parse lines from direction, add test
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- src/day_5.rs
+++ src/day_5.rs
@@ -5,10 +5,11 @@ pub(crate) fn run() {
 }
 
 type Point = (usize, usize);
+type Line = Vec<Point>;
 
-pub(crate) fn parse_lines(inputs: &Vec<String>) -> (Vec<Point>, Vec<Point>) {
-    let hori_lines = Vec::new();
-    let verti_lines = Vec::new();
+pub(crate) fn parse_lines(inputs: &Vec<String>) -> (Vec<Line>, Vec<Line>) {
+    let mut hor_lines = Vec::new();
+    let mut ver_lines = Vec::new();
 
     for input in inputs {
         let start_end = input
@@ -16,19 +17,48 @@ pub(crate) fn parse_lines(inputs: &Vec<String>) -> (Vec<Point>, Vec<Point>) {
             .collect::<Vec<_>>();
 
         let start_str = *start_end.get(0).unwrap();
+        let end_str = *start_end.get(1).unwrap();
 
         let start_point = start_str
             .split(",")
             .map(|n| n.parse::<usize>().unwrap())
             .collect::<Vec<_>>();
+
+        let end_point = end_str
+            .split(",")
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+
+        if start_point[0] == end_point[0] {
+            println!("Line is vertical");
+            // Range over y positions according to direction. Is it away from the origin?
+            let range = match end_point[1] > start_point[1] {
+                true => start_point[1]..end_point[1] + 1,
+                false => end_point[1]..start_point[1] + 1,
+            };
+
+            let line = (range)
+                .map(|y| (start_point[0], y))
+                .collect::<Line>();
+            ver_lines.push(line);
+        } else if start_point[1] == end_point[1] {
+            println!("Line is horizontal");
+            let line = (start_point[0]..end_point[0] + 1)
+                .map(|x| (x, start_point[1]))
+                .collect::<Line>();
+            hor_lines.push(line);
+        }
     }
 
-    (hori_lines, verti_lines)
+    (hor_lines, ver_lines)
 }
 
 #[cfg(test)]
 #[test]
 fn test_straight_line_parser() {
     let inputs = read_lines("data/day_5_sample.txt");
-    let (hori_lines, vert_lines) = parse_lines(&inputs);
+    let (hor_lines, ver_lines) = parse_lines(&inputs);
+
+    assert_eq!(hor_lines[0], vec![(0, 9), (1, 9), (2, 9), (3, 9), (4, 9), (5, 9)]);
+    assert_eq!(ver_lines[0], vec![(2, 1), (2, 2)]);
 }
