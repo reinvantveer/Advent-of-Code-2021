5afd469c9d3cad597552fe10e3f597fdca6eef0d
reinvantveer <rein@vantveer.me>
2021-12-08 19:50:46 +0100

    bootstrap day 7
    Signed-off-by: reinvantveer <rein@vantveer.me>

---
+++ src/day_7.rs
@@ -0,0 +1,10 @@
+use advent_of_code_2021::read_lines;
+
+pub(crate) fn run() {
+}
+
+#[cfg(test)]
+#[test]
+fn test_parse_sub_positions() {
+    assert_eq!(fuel, 37);
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day_3;
 mod day_4;
 mod day_5;
 mod day_6;
+mod day_7;
 
 use clap::{App, Arg};
 
@@ -29,6 +30,7 @@ pub fn main() {
         "4" => { day_4::run() },
         "5" => { day_5::run() },
         "6" => { day_6::run() },
+        "7" => { day_7::run() },
         _ => { todo!("This day isn't implemented (yet)") },
     };
 }
