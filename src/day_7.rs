8600ca583e16499e1a00b64b00038c3e4fd84a50
reinvantveer <reinvantveer@gmail.com>
2021-12-09 10:06:21 +0100

    calculate cheapest alignment from median
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,3 +1,8 @@
+use ndarray::{Array1, Axis};
+use ndarray_stats::interpolate::Nearest;
+use ndarray_stats::QuantileExt;
+use noisy_float::types::n64;
+
 use advent_of_code_2021::{parse_vec_usize, read_lines};
 
 pub(crate) fn run() {
@@ -13,6 +18,17 @@ pub(crate) fn fuel_total_for_alignment(positions: &Vec<usize>, align_position: u
     fuel_total
 }
 
+pub(crate) fn cheapest_alignment(positions: &Vec<usize>) -> usize {
+    let mut vector = Array1::from_vec(positions.clone());
+    let axis = Axis(0);
+    let median = vector
+        .quantile_axis_mut(axis, n64(0.5), &Nearest)
+        .unwrap();
+
+    println!("median: {}", &median);
+    median.as_slice().unwrap()[0]
+}
+
 #[cfg(test)]
 #[test]
 fn test_parse_sub_positions() {
@@ -21,4 +37,12 @@ fn test_parse_sub_positions() {
     let fuel_consumption = fuel_total_for_alignment(&hor_positions, 2);
 
     assert_eq!(fuel_consumption, 37);
+}
+
+#[test]
+fn test_cheapest_alignment() {
+    let inputs = read_lines("data/day_7_sample.txt");
+    let hor_positions = parse_vec_usize(&inputs);
+    let alignment = cheapest_alignment(&hor_positions);
+    assert_eq!(alignment, 2)
 }
