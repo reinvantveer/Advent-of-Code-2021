df7990e1c7a0aeca698cd7f7e2da7d9bdd39095a
reinvantveer <reinvantveer@gmail.com>
2021-12-09 09:19:19 +0100

    add test for fuel consumption calculation
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,10 +1,24 @@
-use advent_of_code_2021::read_lines;
+use advent_of_code_2021::{parse_vec_usize, read_lines};
 
 pub(crate) fn run() {
+}
+
+pub(crate) fn fuel_total_for_alignment(positions: &Vec<usize>, align_position: usize) -> usize {
+    let mut fuel_total = 0;
+
+    for position in positions {
+        fuel_total += (align_position as isize - *position as isize).abs() as usize;
+    }
+
+    fuel_total
 }
 
 #[cfg(test)]
 #[test]
 fn test_parse_sub_positions() {
-    assert_eq!(fuel, 37);
+    let inputs = read_lines("data/day_7_sample.txt");
+    let hor_positions = parse_vec_usize(&inputs);
+    let fuel_consumption = fuel_total_for_alignment(&hor_positions, 2);
+
+    assert_eq!(fuel_consumption, 37);
 }
