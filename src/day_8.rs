66ee9cbcda66e362cda339e6e849b571573d371d
reinvantveer <reinvantveer@gmail.com>
2021-12-09 18:08:01 +0100

    WIP: decode inputs, then decode outputs
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- src/day_8.rs
+++ src/day_8.rs
@@ -1,15 +1,18 @@
+use std::usize;
 use advent_of_code_2021::read_lines;
 
 pub(crate) fn run() {
-    let inputs = read_lines("data/day_8_sample.txt");
+    let inputs = read_lines("data/day_8_input.txt");
     let segment_sets = parse_input_output_signals(&inputs);
     let easy_count = count_easy_segments(&segment_sets);
     println!("The total of easy digits is {}", easy_count);
 }
 
-type SegmentSet = (Vec<String>, Vec<String>);
+type SegmentPatternSet = (Vec<String>, Vec<String>);
+type Signal = Vec<u8>;
+type SegmentSignalSet = (Vec<Signal>, Vec<Signal>);
 
-pub(crate) fn parse_input_output_signals(inputs_outputs: &Vec<String>) -> Vec<SegmentSet> {
+pub(crate) fn parse_input_output_signals(inputs_outputs: &Vec<String>) -> Vec<SegmentPatternSet> {
     let mut segment_sets = Vec::new();
 
     for i_o in inputs_outputs {
@@ -33,7 +36,192 @@ pub(crate) fn parse_input_output_signals(inputs_outputs: &Vec<String>) -> Vec<Se
     segment_sets
 }
 
-pub(crate) fn count_easy_segments(segment_sets: &Vec<SegmentSet>) -> usize {
+pub(crate) fn parse_full_signals(segment_sets: &Vec<SegmentPatternSet>) -> Vec<SegmentSignalSet>{
+    let mut segment_signal_sets = Vec::new();
+
+    for pattern_set in segment_sets {
+        let mut signal_set = (Vec::new(), Vec::new());
+
+        for input in &pattern_set.0 {
+            let signals = input
+                .chars()
+                .map(|c| {
+                    let ascii_number = c.to_string().into_bytes();
+                    ascii_number[0] as u8 - 97
+                })
+                .collect();
+
+            signal_set.0.push(signals)
+        }
+
+        for output in &pattern_set.1 {
+            let signals = output
+                .chars()
+                .map(|c| {
+                    let ascii_number = c.to_string().into_bytes();
+                    ascii_number[0] as u8 - 97
+                } )
+                .collect();
+
+            signal_set.1.push(signals)
+        }
+
+        segment_signal_sets.push(signal_set);
+    }
+
+    segment_signal_sets
+}
+
+///
+//   0:      1:      2:      3:      4:
+//  aaaa    ....    aaaa    aaaa    ....
+// b    c  .    c  .    c  .    c  b    c
+// b    c  .    c  .    c  .    c  b    c
+//  ....    ....    dddd    dddd    dddd
+// e    f  .    f  e    .  .    f  .    f
+// e    f  .    f  e    .  .    f  .    f
+//  gggg    ....    gggg    gggg    ....
+//
+//   5:      6:      7:      8:      9:
+//  aaaa    aaaa    aaaa    aaaa    aaaa
+// b    .  b    .  .    c  b    c  b    c
+// b    .  b    .  .    c  b    c  b    c
+//  dddd    dddd    ....    dddd    dddd
+// .    f  e    f  .    f  e    f  .    f
+// .    f  e    f  .    f  e    f  .    f
+//  gggg    gggg    ....    gggg    gggg
+//
+
+// only one contains  2 segments
+// only four contains 4 segments
+// only seven contains 3 segments
+// only eight contains 7 segments
+
+// three is the only five segment digit containing all segments of the seven
+// nine is the only six-segment digit containing all the elements of the three
+// The bottom left segment is the difference between the eight and the nine
+// five is the only five-segment digit having no bottom left segment
+// two is the only five-segment digit that is not a three and not a five
+// zero is the only six-segment digit that is not a nine and has all the elements of the one
+// six is the only six-segment digit that is not a zero and not a nine
+pub(crate) fn decode_inputs(signal_set: &SegmentSignalSet) -> Vec<Signal> {
+    let inputs = &signal_set.0;
+
+    // only one contains  2 segments
+    let mut one = inputs
+        .iter()
+        .filter(|s| s.len() == 2)
+        .collect::<Vec<_>>()[0]
+        .clone();
+    one.sort();
+
+    // only four contains 4 segments
+    let mut four = inputs
+        .iter()
+        .filter(|s| s.len() == 3)
+        .collect::<Vec<_>>()[0]
+        .clone();
+    four.sort();
+
+    // only the seven contains 3 segments
+    let mut seven = inputs
+        .iter()
+        .filter(|s| s.len() == 3)
+        .collect::<Vec<_>>()[0]
+        .clone();
+    seven.sort();
+
+    // only eight contains 7 segments
+    let mut eight = inputs
+        .iter()
+        .filter(|s| s.len() == 7)
+        .collect::<Vec<_>>()[0]
+        .clone();
+    eight.sort();
+
+    // the three is the only five segment digit containing all segments of the seven
+    let mut three = inputs
+        .iter()
+        .filter(|s| {
+            s.len() == 5
+                && s.contains(&seven[0])
+                && s.contains(&seven[1])
+                && s.contains(&seven[2])
+        })
+        .collect::<Vec<_>>()[0]
+        .clone();
+    three.sort();
+
+    // nine is the only six-segment digit containing all the elements of the three
+    let mut nine = inputs
+        .iter()
+        .filter(|s| {
+            s.len() == 7
+                && s.contains(&three[0])
+                && s.contains(&three[1])
+                && s.contains(&three[2])
+                && s.contains(&three[3])
+                && s.contains(&three[4])
+        })
+        .collect::<Vec<_>>()[0]
+        .clone();
+    nine.sort();
+
+    // The bottom left segment is the only one present in the eight but not the nine
+    let bottom_left = eight
+        .iter()
+        .filter(|num| !nine.contains(num))
+        .collect::<Vec<_>>()[0]
+        .clone();
+
+    // five is the only five-segment digit having no bottom left segment
+    let mut five = inputs
+        .iter()
+        .filter(|input| {
+            input.len() == 5 && !input.contains(&bottom_left)
+        })
+        .collect::<Vec<_>>()[0]
+        .clone();
+    five.sort();
+
+    // two is the only five-segment digit that is not a three and not a five
+    let mut two = inputs
+        .iter()
+        .filter(|input| {
+            *input != &three && *input != &five
+        })
+        .collect::<Vec<_>>()[0]
+        .clone();
+    two.sort();
+
+    // zero is the only six-segment digit that is not a nine and has all the elements of the one
+    let mut zero = inputs
+        .iter()
+        .filter(|input| {
+            *input != &nine && input.contains(&one[0]) && input.contains(&one[1])
+        })
+        .collect::<Vec<_>>()[0]
+        .clone();
+    zero.sort();
+
+    // six is the only six-segment digit that is not a nine and not a zero
+    let mut six = inputs
+        .iter()
+        .filter(|input| {
+            *input != &nine && *input != &zero
+        })
+        .collect::<Vec<_>>()[0]
+        .clone();
+    six.sort();
+
+    vec![one, two, three, four, five, six, seven, eight, nine]
+}
+
+pub(crate) fn decode_outputs(signal_set: &SegmentSignalSet, decoded_inputs: &Vec<Signal>) -> usize {
+    0
+}
+
+pub(crate) fn count_easy_segments(segment_sets: &Vec<SegmentPatternSet>) -> usize {
     let mut count = 0;
 
         for segment_set in segment_sets {
@@ -66,4 +254,25 @@ fn test_count_easy_segments() {
     let segment_sets = parse_input_output_signals(&inputs);
     let count = count_easy_segments(&segment_sets);
     assert_eq!(count, 26);
+}
+
+#[test]
+fn test_full_signal_parse() {
+    let inputs = read_lines("data/day_8_sample.txt");
+    let segment_sets = parse_input_output_signals(&inputs);
+    let segment_signal_sets = parse_full_signals(&segment_sets);
+
+    let first_set = segment_signal_sets[0].clone();
+    assert_eq!(first_set.1[0], vec![5, 3, 6, 0, 2, 1, 4]);
+}
+
+#[test]
+fn test_full_decode() {
+    let inputs = vec!["acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf".to_string()];
+    let segment_sets = parse_input_output_signals(&inputs);
+    let segment_signal_sets = parse_full_signals(&segment_sets);
+
+    let first_set = segment_signal_sets[0].clone();
+    let input_signals =  decode_inputs(&first_set);
+    assert_eq!(display_number, 5353);
 }
