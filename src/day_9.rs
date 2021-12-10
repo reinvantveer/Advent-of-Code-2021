74083916c56aee7dfc97d7c2f9fd0d82413b4ba3
reinvantveer <reinvantveer@gmail.com>
2021-12-10 10:28:05 +0100

    parse digital elevation model, test
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- src/day_9.rs
+++ src/day_9.rs
@@ -1,9 +1,36 @@
+use advent_of_code_2021::read_lines;
+
 pub(crate) fn run() {
 
+}
+
+// A digital elevation model (DEM)
+type DEM = Vec<Vec<usize>>;
+
+pub(crate) fn parse_dem(inputs: &Vec<String>) -> DEM {
+    let mut dem = Vec::new();
+
+    for input in inputs {
+        let row = input
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as usize)
+            .collect();
+        dem.push(row);
+    }
+
+    dem
 }
 
 #[cfg(test)]
 #[test]
 fn test_parse_dem() {
+    let inputs = read_lines("data/day_9_sample.txt");
+    let dem = parse_dem(&inputs);
+    assert_eq!(dem.len(), 5);
 
+    for row in &dem {
+        assert_eq!(row.len(), 10)
+    }
+
+    assert_eq!(dem[4][9], 8)
 }
