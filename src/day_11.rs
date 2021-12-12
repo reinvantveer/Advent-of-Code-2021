cbbc3cf4c899bfce08f77ed5c2ed53fe8643f1f7
reinvantveer <rein@vantveer.me>
2021-12-12 19:11:12 +0100

    implement simple increase, add test for flash
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- src/day_11.rs
+++ src/day_11.rs
@@ -15,6 +15,14 @@ pub(crate) fn octopi_from_input(input: &Vec<String>) -> Vec<Vec<usize>> {
         .collect()
 }
 
+pub(crate) fn simple_energy_increase(octopi: &mut Vec<Vec<usize>>) {
+    for octopus_row in octopi {
+        for octopus in octopus_row {
+            *octopus += 1;
+        }
+    }
+}
+
 #[cfg(test)]
 #[test]
 fn test_parse() {
@@ -22,4 +30,32 @@ fn test_parse() {
     let octopi = octopi_from_input(&input);
     assert_eq!(octopi.len(), 10);
     assert_eq!(octopi[0].len(), 10);
+}
+
+#[test]
+fn test_increase() {
+    let input = read_lines("data/day_11_sample.txt");
+    let mut octopi = octopi_from_input(&input);
+    assert_eq!(octopi[0][0], 5);
+    assert_eq!(octopi[9][9], 6);
+
+    simple_energy_increase(&mut octopi);
+    assert_eq!(octopi[0][0], 6);
+    assert_eq!(octopi[9][9], 7);
+}
+
+#[test]
+fn test_flash() {
+    let input = read_lines("data/day_11_sample.txt");
+    let mut octopi = octopi_from_input(&input);
+    simple_energy_increase(&mut octopi);
+
+    let mut flashes;
+    // Once - nothing happens yet
+    flashes = flash_octopi(&mut octopi);
+    assert_eq!(flashes, 0);
+    // Twice - now
+    flashes = flash_octopi(&mut octopi);
+    assert_eq!(flashes, 35)
+
 }
