9801cf101174408cf728ef0d11464c4a1afb3587
reinvantveer <reinvantveer@gmail.com>
2021-12-13 11:21:55 +0100

    add function to find node index in generic graph, either directed or undirected, add test
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
+use petgraph::{EdgeType, Graph};
+use petgraph::graph::{IndexType, NodeIndex};
 
 pub fn read_lines(file: &str) -> Vec<String> {
     let input = File::open(file).unwrap();
@@ -20,6 +22,17 @@ pub fn parse_vec_usize(inputs: &Vec<String>) -> Vec<usize> {
     vec
 }
 
+pub fn find_node<N, E, D, I>(
+    graph: &Graph<N, E, D, I>,
+    needle: &N
+) -> Option<NodeIndex<I>>
+    where N: PartialEq, D: EdgeType, I: IndexType
+{
+    graph
+        .node_indices()
+        .find(|idx| graph[*idx] == *needle)
+}
+
 #[cfg(test)]
 #[test]
 fn test_vec_usize_from_input() {
@@ -28,3 +41,19 @@ fn test_vec_usize_from_input() {
     assert_eq!(school, vec![3, 4, 3, 1, 2]);
 }
 
+#[test]
+fn test_find_node() {
+    let inputs = read_lines("data/day_9_sample.txt");
+    let dem = parse_dem(&inputs);
+    let dem_graph = graph_from_dem(&dem);
+
+    let global_minimum = DEMPoint{
+        row: 2,
+        column: 2,
+        risk: 6
+    };
+
+    let found_node = find_node(&dem_graph, global_minimum).unwrap();
+    assert_eq!(found_node.index(), 0)
+
+}
