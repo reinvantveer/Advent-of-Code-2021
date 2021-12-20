a2c0ea383a66385365270e3f946a47057b5ace06
reinvantveer <rein@vantveer.me>
2021-12-20 10:19:10 +0100

    add hex crate to parse input packets string
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- src/day_16.rs
+++ src/day_16.rs
@@ -10,6 +10,8 @@ pub(crate) struct Packet {
 
 pub(crate) fn parse_packets(input: &String) -> Vec<Packet>{
     let packets = Vec::new();
+    let bytes = hex::decode(input).unwrap();
+    println!("{:?}", bytes);
 
     packets
 }
@@ -19,4 +21,7 @@ pub(crate) fn parse_packets(input: &String) -> Vec<Packet>{
 fn test_parse_shortest() {
     let input = read_lines("data/day_16_shortest_sample.txt");
     let packets = parse_packets(&input[0]);
+
+    let sum_versions: usize = packets.iter().map(|p| p.version).sum();
+    assert_eq!(sum_versions, 16);
 }
